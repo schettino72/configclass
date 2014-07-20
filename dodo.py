a96bcab2138764ef01d8f6ea7aabadf967e4d458
schettino72 <schettino72@gmail.com>
2014-07-20 22:46:40 +0800

    more doit tasks.

--- dodo.py
+++ dodo.py
@@ -1,4 +1,27 @@
-DOIT_CONFIG = {'default_tasks': []}
+from doitpy.pyflakes import Pyflakes
+
+
+DOIT_CONFIG = {'default_tasks': ['pyflakes']}
+
+
+def task_pyflakes():
+    yield Pyflakes().tasks('**/*.py')
+
+def task_test():
+    return {
+        'actions': ['py.test'],
+        'file_dep': ['configclass.py', 'test_configclass.py'],
+        }
+
+def task_coverage():
+    return {
+        'actions': [
+            'coverage run `which py.test`',
+            'coverage report --include=configclass.py,test_configclass.py'],
+        'verbosity': 2,
+        }
+
+
 
 
 def task_manifest():
